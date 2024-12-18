4660db2e3404b35742b7b82bf56599bba2f374c8
yanghj313 <lawork313@gmail.com>
2024-12-18 17:14:15 +0900

    [update] 메인 2페이지 수정

--- resources/homepage/js/main.js
+++ resources/homepage/js/main.js
@@ -56,3 +56,58 @@ $(function () {
 		autoControls: false,
 	});
 });
+
+$(document).ready(function () {
+	// 팝업 스와이퍼 페이지네이션
+	let slidesPerPage = 1;
+	const totalSlides = $('.popup_swiper .swiper-slide').length;
+	let totalPages = Math.ceil(totalSlides / slidesPerPage);
+
+	// 팝업 스와이퍼
+	const popupSwiper = new Swiper('.popup_swiper .swiper', {
+		speed: 3000,
+		pagination: {
+			el: '.popup_swiper .swiper-pagination',
+			type: 'fraction',
+			renderFraction: function (currentClass, totalClass) {
+				return '<span class="' + currentClass + '"></span>' + ' / ' + totalPages;
+			},
+		},
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false,
+		},
+		loop: true,
+		slidesPerView: 1,
+		loopAddBlankSlides: true,
+		navigation: {
+			nextEl: '.popup_swiper .swiper-button-next',
+			prevEl: '.popup_swiper .swiper-button-prev',
+		},
+	});
+
+	// 팝업 스와이퍼 정지 OR 재생
+	var popzoneConLen = $('.popup_swiper .swiper-slide').length;
+	if (popzoneConLen > 1) {
+		const autoplayButton = document.querySelector('.popup_autoplay');
+
+		autoplayButton.addEventListener('click', () => {
+			if (popupSwiper.autoplay.running) {
+				popupSwiper.autoplay.stop();
+				updateAutoplayButton('../../../../resources/homepage/img/common/play.svg', '재생버튼');
+			} else {
+				popupSwiper.autoplay.start();
+				updateAutoplayButton('../../../../resources/homepage/img/common/stop.svg', '정지버튼');
+			}
+		});
+
+		function updateAutoplayButton(src, alt) {
+			autoplayButton.src = src;
+			autoplayButton.alt = alt;
+		}
+	}
+
+	/* ------------------------------------------------------------------------------------------------------------------- */
+});
+
+ur;
